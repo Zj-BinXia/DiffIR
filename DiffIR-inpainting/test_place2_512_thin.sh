46a9ae9627505339cce0b2f6ec3bb7dc78205927
Zj-BinXia <95993353+Zj-BinXia@users.noreply.github.com>
2023-08-21 21:23:18 +0800

    Update test_place2_512_thin.sh

--- DiffIR-inpainting/test_place2_512_thin.sh
+++ DiffIR-inpainting/test_place2_512_thin.sh
@@ -1,4 +1,4 @@
 CUDA_VISIBLE_DEVICES=3 python3 bin/predict.py \
-model.path=/mnt/bn/xiabinpaint/ICCV-Inpainting/KDSR-inpainting-lightv2/experiments/inpainting_2023-02-14_22-21-15_train_DiffIRS-place2_/ \
+model.path=$(pwd)/experiments/DiffIRS2-place/ \
 indir=/mnt/bn/xiabinpaint/dataset/inpainting/place2/evaluation/random_thin_512/ \
 outdir=$(pwd)/inference/random_thiin_512 model.checkpoint=last.ckpt
