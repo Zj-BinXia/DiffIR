c769eff87f5da3025d7f396b1b065ad79a0f770e
Zj-BinXia <95993353+Zj-BinXia@users.noreply.github.com>
2023-08-21 20:52:30 +0800

    Update S1forS2.py

--- DiffIR-inpainting/S1forS2.py
+++ DiffIR-inpainting/S1forS2.py
@@ -4,7 +4,7 @@ save_path = "./celeba-S1.pth"
 #path = "./experiments/DiffIRS1-place/models/last.ckpt"
 #save_path = "./place-S1.pth"
 #path = "./experiments/Big-DiffIRS1-place/models/last.ckpt"
-#save_path = "./placebig-S1.pth"
+#save_path = "./placebigdata-S1.pth"
 s=torch.load(path)
 # for k,v in s.items():
 #    print(k)
