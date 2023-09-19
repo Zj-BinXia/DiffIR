88b7d441418b768dcc5f4677ce8f05cf7a07740e
Zj-BinXia <95993353+Zj-BinXia@users.noreply.github.com>
2023-09-19 10:48:42 +0800

    Update train_DiffIRS2.sh

--- DiffIR-RealSR/train_DiffIRS2.sh
+++ DiffIR-RealSR/train_DiffIRS2.sh
@@ -1,6 +1,8 @@
-
-
-
-
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
 python3  -m torch.distributed.launch --nproc_per_node=8 --master_port=5685 DiffIR/train.py -opt options/train_DiffIRS2_x4.yml --launcher pytorch 
+
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
+# python3  -m torch.distributed.launch --nproc_per_node=8 --master_port=5685 DiffIR/train.py -opt options/train_DiffIRS2_x2.yml --launcher pytorch 
+
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
+# python3  -m torch.distributed.launch --nproc_per_node=8 --master_port=5685 DiffIR/train.py -opt options/train_DiffIRS2_x1.yml --launcher pytorch 
