db985decdc042f0022b4a013495854075d6ee63e
Zj-BinXia <95993353+Zj-BinXia@users.noreply.github.com>
2023-09-19 10:51:27 +0800

    Create train_DiffIRS2_GANv2.sh

---
+++ DiffIR-RealSR/train_DiffIRS2_GANv2.sh
@@ -0,0 +1,8 @@
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
+python3 -m torch.distributed.launch --nproc_per_node=8 --master_port=4397 DiffIR/train.py -opt options/train_DiffIRS2_GAN_x4_V2.yml --launcher pytorch
+
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
+# python3 -m torch.distributed.launch --nproc_per_node=8 --master_port=4397 DiffIR/train.py -opt options/train_DiffIRS2_GAN_x2_V2.yml --launcher pytorch
+
+# CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
+# python3 -m torch.distributed.launch --nproc_per_node=8 --master_port=4397 DiffIR/train.py -opt options/train_DiffIRS2_GAN_x1_V2.yml --launcher pytorch
